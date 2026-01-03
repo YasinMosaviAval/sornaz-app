cc63541551921d19e6f33f53ca35711a86e901db
YasinMosaviAval <yasin.mosaviaval@gmail.com>
2026-01-03 23:54:10 +0330

    - Fixed page scrolling issue - Fixed playback speed changes - Fixed song selection in folder section

--- lib/audio/cache/hive_audio_cache.dart
+++ lib/audio/cache/hive_audio_cache.dart
@@ -8,7 +8,6 @@ class HiveAudioCache implements AudioCacheService {
 
   @override
   Future<void> init() async {
-    // await Hive.initFlutter();
     _box = await Hive.openBox<AudioFileHive>('audio_files_hive');
   }
 
